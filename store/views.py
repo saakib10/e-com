2c4fc6ac0fd7122a875d9b8bf620104f78939557
Saakib10 <saakib10@yahoo.com>
2022-11-03 16:49:05 +0600

    chore: user order list get from database

--- store/views.py
+++ store/views.py
@@ -157,17 +157,21 @@ def view_details(request,id):
 
 def price(request):
     if request.user.is_authenticated:
+        context= {}
         customer = request.user.customer
-        print("Pppppppppppppp",customer)
-        item = Product.objects.values()
-        user_items = list(item)
-        return JsonResponse({'items': user_items})
-    return render(request,'user_order_details.html')
-
-    if request.method == 'GET':
-        selectedProduct = request.GET['client_response']
-        total_price = Product.objects.filter(product = selectedProduct).first().price
-        response_data ={}
-        response_data['price'] = total_price
-
-        return JsonResponse(response_data)
+        orders = Order.objects.filter(customer=customer, complete = True).values()
+        context['total_order'] = len(orders)
+        if orders:
+            items = []
+            for x in orders:
+                order_items = OrderItem.objects.filter(order = x.get("id")).values()
+                for or_itms in order_items:
+                    data_dict = {}
+                    item = Product.objects.get(id = or_itms.get("product_id"))
+                    data_dict["item_name"] = item.name
+                    data_dict["qty"] = or_itms.get("quantity")
+                    data_dict["order"] = x.get("id")
+                    items.append(data_dict)
+            context["items"] = items
+        print(context)
+    return render(request,'user_order_details.html',context)
