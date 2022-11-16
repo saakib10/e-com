74af12cf3ac125c68931fa620d684d03a529956a
Saakib10 <saakib10@yahoo.com>
2022-11-16 17:39:13 +0600

    chore: 16-11-22 Office 1

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
@@ -8,7 +8,9 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.http import JsonResponse
 from django.db.models import Q
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.template.defaulttags import register
 
 
 def home(request):
@@ -34,9 +36,10 @@ def home(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = 0
     s_image = HomepageSlideshow.objects.all()
+    top_sell_item = Product.objects.all().order_by("-total_orders")[:6]
     
     category = Category.objects.all()
-    contex = {'items': items,'order':order,'cartItems':cartItems,'images':s_image,'category':category,'products':items or []}
+    contex = {'items': items,'order':order,'cartItems':cartItems,'images':s_image,'category':category,'products':items or [],"top_sells":top_sell_item}
     return render(request,'home.html',contex)
 
 def cart(request):
@@ -49,28 +52,12 @@ def cart(request):
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = 0
-        
-        
-        
-    n_c_list = []  
-    n_c_order = Order.objects.filter(complete = False).values()
-    for x in n_c_order:
-        print(type(x.get("id")))
-        n_c_list.append(x.get("id"))
-    # order_item = OrderItem
-    print(n_c_list)
-    order_items = OrderItem.objects.all()
-    for x in order_items:
-        if x.order.id not in n_c_list:
-            print(x.order)
-
-        
-        
-        
     
     contex = {'items': items,'order':order,'cartItems':cartItems}
     return render(request,'cart.html',contex)
 
+
+    
 def shop(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -83,15 +70,38 @@ def shop(request):
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     category = Category.get_all_category()
+    
     products = None
     categoryID= request.GET.get('category')
-
+    item_show = request.GET.get('item_number') or 5
+    pagina = get_pagination_number(item_show)
+    
+    # paginator = get_range(pagina)
+    
     if categoryID:
-        products = Product.get_all_product_by_category_id(categoryID)
-    else:
-        products = Product.get_all_products()
+        products_all = Product.get_all_product_by_category_id(categoryID)
+        page = request.GET.get('page') or 1
 
-    contex = {'items': items,'products':products,'cartItems':cartItems,'order':order,'category':category}
+        paginator = Paginator(products_all,5)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+    else:
+        products_all = Product.get_all_products()
+        page = request.GET.get('page') or 1
+
+        paginator = Paginator(products_all,5)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+
+    contex = {'items': items,'products':products,'cartItems':cartItems,'order':order,'category':category,"paginator":pagina}
     return render(request,'shop.html',contex)
 
 def checkout(request):
@@ -160,21 +170,27 @@ def processOrder(request):
 
         if total == order.get_cart_total:
             order.complete = True
+            orderitems = OrderItem.objects.filter(order = order.id).values()
+            if orderitems:
+                for item in orderitems:
+                    product = Product.objects.get(id = item.get("product_id"))
+                    product.total_orders += item.get("quantity")
+                    product.save()
         order.save()
 
-        template = render_to_string('store/order.html',{'name':request.user.username})
-        subject = 'Order Confirmations'
-        # body = 'Your Account is Activated'
-        from_mail = settings.EMAIL_HOST_USER
-        to_mail = [request.user.email]
-        send_mail(
-            subject,
-            # body,
-            template,
-            from_mail,
-            to_mail,
-        )
-        send_mail(subject, template, from_mail, to_mail, fail_silently=False)
+        # template = render_to_string('store/order.html',{'name':request.user.username})
+        # subject = 'Order Confirmations'
+        # # body = 'Your Account is Activated'
+        # from_mail = settings.EMAIL_HOST_USER
+        # to_mail = [request.user.email]
+        # send_mail(
+        #     subject,
+        #     # body,
+        #     template,
+        #     from_mail,
+        #     to_mail,
+        # )
+        # send_mail(subject, template, from_mail, to_mail, fail_silently=False)
 
     else:
         print('user is not logged in ')
@@ -210,3 +226,28 @@ def item_popup_details(request):
         print(product.imagesURL)
         item = {"id":product.id,'name':product.name,"image":product.imagesURL,"price":product.price,"description":product.descriptions}
     return JsonResponse({'data': item})
+
+
+def update_ordered_product_quantity():
+    n_c_list = []  
+    n_c_order = Order.objects.filter(complete = False).values()
+    for x in n_c_order:
+        print(type(x.get("id")))
+        n_c_list.append(x.get("id"))
+    # order_item = OrderItem
+    order_items = OrderItem.objects.all()
+    for x in order_items:
+        if x.order.id not in n_c_list:
+            product = Product.objects.get(id = x.product.id)
+            product.total_orders += x.quantity
+            product.save()
+            
+def get_pagination_number(item_show):
+    product = Product.objects.all().count()
+    return round(product/item_show)
+
+
+# Paginator Count
+@register.filter
+def get_range(pagination):
+    return range(1,pagination+2)
